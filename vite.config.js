9f4725213496242a784b912ab9872876b68b8a48
Mitanshu Badgujar <119037058+Mitanshubad@users.noreply.github.com>
2024-07-04 00:06:43 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,17 +3,9 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  build: {
-    rollupOptions: {
-      external: ['@google/generative-ai', 'jspdf'] // Add jspdf as well if external
-    }
-  },
   resolve: {
     alias: {
       '@': '/src' // Custom alias for your src folder
     }
-  },
-  optimizeDeps: {
-    exclude: ['@google/generative-ai'] // Exclude from optimization
   }
 });
